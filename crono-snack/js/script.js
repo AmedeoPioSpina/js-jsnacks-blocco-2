6b5093624d100baca3c325100f50c60b80ebf56c
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-11-23 12:29:28 +0100

    commit

---
+++ crono-snack/js/script.js
@@ -0,0 +1,15 @@
+let tempo = () => {
+    sec += 1;
+    console.log(sec);
+}
+
+let sec = 0;
+
+let btnList = document.querySelectorAll("button");
+
+btnList[0].addEventListener("click", () => {
+    let cronometro = setInterval(tempo, 1000, 0);
+    btnList[1].addEventListener("click", () => {
+        clearInterval(cronometro);
+    });
+})
