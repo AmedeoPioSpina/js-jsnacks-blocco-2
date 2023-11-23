67c9af8871f4af73d98d7a65bab4ae38e158b386
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-11-23 12:30:27 +0100

    fix

--- crono-snack/js/script.js
+++ crono-snack/js/script.js
@@ -4,12 +4,12 @@ let tempo = () => {
 }
 
 let sec = 0;
-
 let btnList = document.querySelectorAll("button");
 
 btnList[0].addEventListener("click", () => {
     let cronometro = setInterval(tempo, 1000, 0);
     btnList[1].addEventListener("click", () => {
         clearInterval(cronometro);
+        sec = 0;
     });
 })
